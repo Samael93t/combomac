40ff4a12f89584851a08acfee548d48d46a4af70
Samael93t <snaker.1993@gmail.com>
2025-05-18 19:55:47 -0500

    Add files via upload

---
+++ generadormac.py
@@ -0,0 +1,142 @@
+#Combos Mac Iptv Samael
+
+
+yeninesil=(
+"00:1A:79:",
+"33:44:CF:",
+"10:27:BE:",
+"A0:BB:3E:",
+"00:1a:79:",
+)
+
+import random
+import subprocess,webbrowser
+import time
+import sys
+subprocess.run(["clear", ""])
+
+
+texto1 = """╔══╗─╔╗─────╔╦═╗
+║═╦╬╦╬╬═╦═╦╦╝║═╣
+║╔╝║╔╣║╩╣║║║╬╠═║
+╚╝─╚╝╚╩═╩╩═╩═╩═╝"""
+
+texto2 = """╔══╗─╔╗────╔╗
+║══╬═╣╚╦═╦═╣║
+╠══║═╣║║╬║╬║║
+╚══╩═╩╩╩═╩═╩╝"""
+
+print("\033[34m" + texto1 + "\033[0m")  # Azul
+print("\033[35m" + texto2 + "\033[0m")  # Morado
+
+sam = """▄▀▀ ▄▀▄ █▄░▄█ ▄▀▄ █▀ █░░
+░▀▄ █▀█ █░█░█ █▀█ █▀ █░░
+▀▀░ ▀░▀ ▀░░░▀ ▀░▀ ▀▀ ▀▀▀"""
+
+print("\033[34m" + sam + "\033[0m")  # Azul
+print("\33[93m" + """⣿⣿⠿⠿⠿⠿⣿⣷⣂⠄⠄⠄⠄⠄⠄⠈⢷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣷⡾⠯⠉⠉⠉⠉⠚⠑⠄⡀⠄⠄⠄⠄⠄⠈⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣿⣿⣿⣿⣿
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⠎⠄⠄⣀⡀⠄⠄⠄⠄⠄⠄⠄⠘⠋⠉⠉⠉⠉⠭
+⡀⠄⠄⠄⠄⠄⠄⠄⠄⡇⠄⣠⣾⣳⠁⠄⠄⢺⡆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⣿⣷⡦⠄⠄⠄⠄⠄⢠⠃⢰⣿⣯⣿⡁⢔⡒⣶⣯⡄⢀⢄⡄⠄⠄⠄⠄⠄⣀
+⠓⠄⠄⠄⠄⠄⠸⠄⢀⣤⢘⣿⣿⣷⣷⣿⠛⣾⣿⣿⣆⠾⣷⠄⠄⠄⠄⢀⣀
+⠄⠄⠄⠄⠄⠄⠄⠑⢘⣿⢰⡟⣿⣿⣷⣫⣭⣿⣾⣿⣿⣴⠏⠄⠄⢀⣺⣿⣿
+⣿⣿⣿⣿⣷⠶⠄⠄⠄⠹⣮⣹⡘⠛⠿⣫⣾⣿⣿⣿⡇⠑⢤⣶⣿⣿⣿⣿⣿
+⣿⣿⣿⣯⣤⣤⣤⣤⣀⣀⡹⣿⣿⣷⣯⣽⣿⣿⡿⣋⣴⡀⠈⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣝⡻⢿⣿⡿⠋⡒⣾⣿⣧⢰⢿⣿⣿⣿⣿⣿
+""" + "\33[0m")
+# Color morado
+print("\033[95m", end='')
+texto1 = "៚⋞×͜͡🔥𝐅𝐑𝐈𝐄𝐍𝐃𝐒 𝐒𝐂𝐇𝐎𝐎𝐋🔥⋟࿐💪"
+for char in texto1:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.05)
+print()
+
+# Color amarillo
+print("\033[93m", end='')
+texto2 = "▰▰▰▰ GENERADOR DE MAC 💪 ▰▰▰▰"
+for char in texto2:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.05)
+print()
+
+# Color naranja (no hay un código específico para naranja, pero podemos usar el código para el color rojo claro)
+print("\033[38;5;208m", end='')
+texto3 = "▰▰ ៚⋞×͜͡🔥𝐒ᴀᷧᴍͩᴀᷟᴇʟᷫ×ᷤ🔥⋟࿐▰▰"
+for char in texto3:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.05)
+print()
+
+# Restablecer el color
+print("\033[0m")
+print("""
+
+Tipos de Mac disponibles:
+""")
+nnesil=str(yeninesil)
+nnesil=(nnesil.count(',')+1)
+for xd in range(0,(nnesil)):
+		print(str(xd+1)+" - "+yeninesil[xd] )
+#subprocess.run(["clear", ""])
+
+
+#print(nnesil)
+i=0
+
+dosya=input("""Crear el nombre de su combo...
+
+sin colocar  \33[1;31;40m.txt\33[0m\33[32m
+
+
+ Nombre de su combo=""")
+karisik=input("""
+\33[33m1\n       👉!!!! 🅓🅐🅛🅔 🅔🅝🅣🅔🅡 !!!! 👈                          """)
+karisik=karisik.upper()
+print("")
+if not karisik[:1]=="E" :
+	for xd in range(0,(nnesil)):
+		print(str(xd+1)+" - "+yeninesil[xd] )
+	nesil=input("""\33[36m\nElegir el tipo de Mac=""") 
+
+adet=input("""
+
+Cuantos mac quiere generar? =""")
+
+DosyaA="/sdcard/combo/" +dosya+".txt"
+def kaydet(mac):
+    dosya=open(DosyaA,'a+') 
+    dosya.write(mac)
+    dosya.close()
+
+
+while True:
+	#hex_num = hex(mac)[2:].zfill(6)
+	genmac = "%02x:%02x:%02x"% (random.randint(0, 256),random.randint(0, 256),random.randint(0, 256))
+	genmac=genmac.replace('100','10')
+	genmac=genmac.lower()
+	#print(karisik[:1])
+	if karisik[:1]=="E" :
+		for xd in range(0,nnesil):
+				genmac = "%02x:%02x:%02x"% (random.randint(0, 256),random.randint(0, 256),random.randint(0, 256))
+				genmac=genmac.replace('100','10')
+				print(yeninesil[xd]+genmac)
+				kaydet(yeninesil[xd]+genmac+"\n")
+	else:
+		print(yeninesil[int(nesil)-1]+genmac)
+		kaydet(yeninesil[int(nesil)-1]+genmac+"\n")
+		i=i+1			
+	
+	if str(i) ==adet:
+		break
+
+print("\n\n Processo completo, aproveche su combo!\n\n")
+
+print("Archivo salvado en sdcard/combo/")
+	
+print("♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️")	
